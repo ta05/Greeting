4a9626748bf1ca248fd58e0c41757add3936f4c1
Tolu Alimi <tolualimi05@gmail.com>
2020-09-07 14:58:15 -0500

    Changed asgi file

--- greeting/asgi.py
+++ greeting/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greeting.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gettingstarted.settings.base')
 
 application = get_asgi_application()
