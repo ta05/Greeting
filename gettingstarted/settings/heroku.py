d22891894d18782ebac15a3cd998fd901b08ab26
Tolu Alimi <tolualimi05@gmail.com>
2020-09-07 19:07:02 -0500

    Added production postgress db

--- gettingstarted/settings/heroku.py
+++ gettingstarted/settings/heroku.py
@@ -3,6 +3,8 @@ Production Settings for Heroku
 """
 
 import environ
+import dj_database_url 
+
 
 # If using in your own project, update the project namespace below
 from gettingstarted.settings.base import *
@@ -20,8 +22,6 @@ SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
+DATABASE_URL = env('DATABASE_URL')
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
-DATABASES = {
-    # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
-    'default': env.db(),
-}
+DATABASES['default'] = dj_database_url.parse('postgres://jsefvoapkecivr:4009246d6062f477a9c09a3f16e578d4f01968dd0f367613ece3829ea893e52b@ec2-52-202-66-191.compute-1.amazonaws.com:5432/d7m28n3sitso46', conn_max_age=500)
